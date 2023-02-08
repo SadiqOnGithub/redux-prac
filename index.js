0f1d371e0816a2ef49c08e06843437f4f5c1ab4d
SYED SADIQ <sadiqonemail@gmail.com>
2023-02-08 17:23:27 +0530

    multiple actions in single reducer - codevolution

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+/* 
+
+-redux template
+-multiple actions in single reducer
+
+*/
+
+
 const redux = require('redux');
-const createStore = redux.createStore;
+const { createStore, bindActionCreators } = redux;
 
 // actions
 const CAKE_ORDERED = 'CAKE_ORDERED';
 const CAKE_RESTOCKING = 'CAKE_RESTOCKING';
+const ICECREAM_ORDERED = 'ICECREAM_ORDERED';
+const ICECREAM_RESTOCKING = 'ICECREAM_RESTOCKING';
 
 // action creator
-function orderCake() {
+function orderCake(qty = 1) {
 	return {
 		type: CAKE_ORDERED,
-		payload: 1
+		payload: qty
 	}
 }
 
@@ -20,10 +30,24 @@ function restockCake(qty = 1) {
 	}
 }
 
+function orderIceCream(qty = 1) {
+	return {
+		type: ICECREAM_ORDERED,
+		payload: qty
+	}
+}
+
+function restockIceCream(qty = 1) {
+	return {
+		type: ICECREAM_RESTOCKING,
+		payload: qty
+	}
+}
+
 // initial state
 const initialState = {
 	numOfCakes: 10,
-	anotherProp: 0
+	numOfIceCreams: 20
 }
 
 // reducer func
@@ -39,6 +63,16 @@ const reducer = (state = initialState, action) => {
 				...state,
 				numOfCakes: state.numOfCakes + action.payload
 			}
+		case ICECREAM_ORDERED:
+			return {
+				...state,
+				numOfIceCreams: state.numOfIceCreams - 1
+			}
+		case ICECREAM_RESTOCKING:
+			return {
+				...state,
+				numOfIceCreams: state.numOfIceCreams + action.payload
+			}
 		default:
 			return state
 	}
@@ -51,8 +85,23 @@ console.log('initial state', store.getState());
 
 const unsubscribe = store.subscribe(() => console.log('updated state', store.getState()))
 
-store.dispatch(orderCake())
-store.dispatch(orderCake())
-store.dispatch(orderCake())
-store.dispatch(restockCake(3))
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(restockCake(3))
+
+const action = bindActionCreators(
+	{ orderCake, restockCake, orderIceCream, restockIceCream },
+	store.dispatch
+)
+
+action.orderCake()
+action.orderIceCream()
+action.orderCake()
+action.orderIceCream()
+action.orderCake()
+action.orderIceCream()
+action.restockCake(3)
+action.restockIceCream(3)
+
 unsubscribe();
